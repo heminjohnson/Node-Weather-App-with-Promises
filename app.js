2968df5f77e4628be08f5d92e542d4caab826f2e
Hemin Johnson <hemin.johnson@mieterengel.de>
2018-10-16 00:13:47 +0200

    feat: add weather feature

--- app.js
+++ app.js
@@ -17,7 +17,18 @@ axios.get(geoCodeUrl).then((response) => {
   if (response.data.info.status === 400) {
     console.log('please provide an address')
   }
-  console.log(response.data.results)
+  var latitude = response.data.results[0].locations[0].latLng.lat
+  var longitude = response.data.results[0].locations[0].latLng.lng
+  var address = response.data.results[0].providedLocation.location
+  console.log(`Address: ${address}`)
+  console.log(`Latitude: ${latitude}`)
+  console.log(`Longitude ${longitude}`)
+  var WeatherUrl = `https://api.darksky.net/forecast/KEY/${latitude},${longitude}`
+  return axios.get(WeatherUrl)
+}).then((response) => {
+  var temperature = response.data.currently.temperature
+  var apparentTemperature = response.data.currently.apparentTemperature
+  console.log(`It is currently ${temperature} and it feels like ${apparentTemperature}`)
 }).catch((e) => {
   console.log(e)
 })
